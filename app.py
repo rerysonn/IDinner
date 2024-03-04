571f6634195be0b74bdf7d7fad59ec7efd5ff8f0
Reryson Farinha <rerysonfarinha@outlook.com>
2024-03-04 20:30:04 -0300

    IDinner - Funcao Escolha

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 # Abertura do app
-print("𝙸𝙳𝚒𝚗𝚗𝚎𝚛")
+print("𝙱𝚎𝚖 𝚟𝚒𝚗𝚍𝚘 𝚊𝚘 𝙸𝙳𝚒𝚗𝚗𝚎𝚛\n")
+
+def opcoes():
+    print("1 - Cadastrar restaurante\n2 - Listar restaurante\n3 - Ativar restaurante\n4 - Sair do app")
+    escolha = int(input("Escolha uma das opcoes: "))
+
+    if escolha == 1:
+        print("Cadastro de restaurante")
+    elif escolha == 2:
+        print("Listar Restaurantes")
+    elif escolha == 3:
+        print("Ativando Restaurantes")
+    elif escolha == 4:
+        print("Saindo do app")
+    else:
+        print("Opção invalida")
+    
+opcoes()
 
