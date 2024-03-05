d176b37adf59b121b21c4281aaab210cba17abaf
Reryson Farinha <rerysonfarinha@outlook.com>
2024-03-05 09:15:25 -0300

    IDinner - Cadastro de restaurantes

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 import os
 
 # Abertura do app
-print("𝙱𝚎𝚖 𝚟𝚒𝚗𝚍𝚘 𝚊𝚘 𝙸𝙳𝚒𝚗𝚗𝚎𝚛\n")
+def nome_programa():
+    print("𝙱𝚎𝚖 𝚟𝚒𝚗𝚍𝚘 𝚊𝚘 𝙸𝙳𝚒𝚗𝚗𝚎𝚛\n")
 
-
+# Print das funcionalidades do app
 def opcoes():
     print("1 - Cadastrar restaurante\n2 - Listar restaurante\n3 - Ativar restaurante\n4 - Sair do app")
 
-    
+def cadastrar_restaurantes():
+    os.system("cls")
+    print("Cadastro de restaurante\n")
+    nome_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
+    print(f"O restaurante {nome_restaurante} foi adicionado com sucesso\n")
+    input("Digite alguma tecla para voltar a tela inicial ")
+    voltar_ao_menu()
+
+def voltar_ao_menu():
+    os.system("cls")
+    nome_programa()
+    opcoes()
+    escolha_opcoes()
+
+
+# Logica para escolha da funcionalidade   
 def escolha_opcoes():
     escolha = int(input("Escolha uma das opcoes: "))
 
     if escolha == 1:
-        os.system("cls")
-        print("Cadastro de restaurante")
+        cadastrar_restaurantes()
     elif escolha == 2:
         os.system("cls")
         print("Listar Restaurantes")
@@ -24,9 +39,10 @@ def escolha_opcoes():
         os.system("cls")
         print("Saindo do app")
     else:
-        print("Opção invalida")
+        print("\nOpção invalida")
     
 def sistema():
+    nome_programa()
     opcoes()
     escolha_opcoes()
 
